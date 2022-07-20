eb382ecb9962157f0fd398e60e5718884a3d303f
Elijah Odjegba <elijahodjegba@gmail.com>
2022-07-20 12:06:02 +0100

    Add hamburger menu

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
-const brandName = document.querySelector('.nav-branding');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
-  // brandName.style.display = "none";
 });
 
-document.querySelectorAll('.nav-link').forEach((n) =>
-  n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-  })
-);
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
