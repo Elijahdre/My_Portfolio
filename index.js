44fafa4ca46e32e099e40c414286233f7a0cc7d4
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-07-20 12:13:36 +0300

    Solve Hamburger menu

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const brandName = document.querySelector(".nav-branding");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const brandName = document.querySelector('.nav-branding');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
   // brandName.style.display = "none";
+});
 
-})
-
-
-document.querySelectorAll(".nav-link").forEach(n => n.
-addEventListener("click", () => {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-}))
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  })
+);
